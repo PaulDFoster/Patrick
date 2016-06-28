ff3c0cbe084f226f032fde859aeb00bbef9bff24
Paul Foster <paulfo@microsoft.com>
2016-06-28 11:36:01 +0100

    Added Alljoyn and PWM projects
    Added required project dependencies for Alljoyn SmartSwitch and
    PWMSoftware for servo control

---
+++ AllJoynApp1/AllJoynApp1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.AllJoyn;
+using com.microsoft.ZWaveBridge.SwitchBinary.Switch;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AllJoynApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SwitchConsumer switchConsumer;
+        bool lightStatus = false;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AllJoynBusAttachment switchBusAttachment = new AllJoynBusAttachment();
+            SwitchWatcher switchWatcher = new SwitchWatcher(switchBusAttachment);
+            switchWatcher.Added += SwitchWatcher_Added;
+            switchWatcher.Start();
+        }
+
+        private async void SwitchWatcher_Added(SwitchWatcher sender, AllJoynServiceInfo args)
+        {
+            
+            SwitchJoinSessionResult joinSessionResult = await SwitchConsumer.JoinSessionAsync(args, sender);
+            if (joinSessionResult.Status == AllJoynStatus.Ok)
+            {
+                switchConsumer = joinSessionResult.Consumer;
+                btnLightSwitch.IsEnabled = true;
+            }
+        }
+
+        private async void btnLightSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            lightStatus = !lightStatus;
+            await switchConsumer.SetValueAsync(lightStatus);
+        }
+    }
+}
